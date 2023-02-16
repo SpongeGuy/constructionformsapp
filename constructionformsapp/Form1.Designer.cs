766b069f8e5c9d80098cd79e73ee554aedd725cd
realevan03@gmail.com <unconfigured@null.spigotmc.org>
2023-02-15 19:35:59 -0500

    added content to datagridview and made submit button clear fields

--- constructionformsapp/Form1.Designer.cs
+++ constructionformsapp/Form1.Designer.cs
@@ -29,14 +29,14 @@ namespace constructionformsapp
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.listCategory = new System.Windows.Forms.ComboBox();
+            this.textItem = new System.Windows.Forms.TextBox();
+            this.textMaterial = new System.Windows.Forms.TextBox();
+            this.textDescription = new System.Windows.Forms.TextBox();
+            this.textQuantity = new System.Windows.Forms.TextBox();
+            this.textUnitCost = new System.Windows.Forms.TextBox();
+            this.textCost = new System.Windows.Forms.TextBox();
+            this.textNotes = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,68 +45,76 @@ namespace constructionformsapp
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSubmit = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // listCategory
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 26);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(118, 23);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.listCategory.FormattingEnabled = true;
+            this.listCategory.Items.AddRange(new object[] {
+            "Foundation",
+            "Framing",
+            "Cladding",
+            "Roofing",
+            "Flooring",
+            "Interior Walls",
+            "Openings"});
+            this.listCategory.Location = new System.Drawing.Point(12, 26);
+            this.listCategory.Name = "listCategory";
+            this.listCategory.Size = new System.Drawing.Size(118, 23);
+            this.listCategory.TabIndex = 0;
+            this.listCategory.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // textBox1
+            // textItem
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 23);
-            this.textBox1.TabIndex = 2;
+            this.textItem.Location = new System.Drawing.Point(137, 26);
+            this.textItem.Name = "textItem";
+            this.textItem.Size = new System.Drawing.Size(132, 23);
+            this.textItem.TabIndex = 2;
             // 
-            // textBox2
+            // textMaterial
             // 
-            this.textBox2.Location = new System.Drawing.Point(275, 26);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(197, 23);
-            this.textBox2.TabIndex = 3;
+            this.textMaterial.Location = new System.Drawing.Point(275, 26);
+            this.textMaterial.Name = "textMaterial";
+            this.textMaterial.Size = new System.Drawing.Size(197, 23);
+            this.textMaterial.TabIndex = 3;
             // 
-            // textBox3
+            // textDescription
             // 
-            this.textBox3.Location = new System.Drawing.Point(478, 26);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(270, 23);
-            this.textBox3.TabIndex = 4;
+            this.textDescription.Location = new System.Drawing.Point(478, 26);
+            this.textDescription.Name = "textDescription";
+            this.textDescription.Size = new System.Drawing.Size(270, 23);
+            this.textDescription.TabIndex = 4;
             // 
-            // textBox4
+            // textQuantity
             // 
-            this.textBox4.Location = new System.Drawing.Point(754, 26);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(73, 23);
-            this.textBox4.TabIndex = 5;
+            this.textQuantity.Location = new System.Drawing.Point(754, 26);
+            this.textQuantity.Name = "textQuantity";
+            this.textQuantity.Size = new System.Drawing.Size(73, 23);
+            this.textQuantity.TabIndex = 5;
             // 
-            // textBox5
+            // textUnitCost
             // 
-            this.textBox5.Location = new System.Drawing.Point(833, 26);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(72, 23);
-            this.textBox5.TabIndex = 6;
+            this.textUnitCost.Location = new System.Drawing.Point(833, 26);
+            this.textUnitCost.Name = "textUnitCost";
+            this.textUnitCost.Size = new System.Drawing.Size(72, 23);
+            this.textUnitCost.TabIndex = 6;
             // 
-            // textBox6
+            // textCost
             // 
-            this.textBox6.Location = new System.Drawing.Point(911, 26);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 23);
-            this.textBox6.TabIndex = 7;
+            this.textCost.Location = new System.Drawing.Point(911, 26);
+            this.textCost.Name = "textCost";
+            this.textCost.Size = new System.Drawing.Size(100, 23);
+            this.textCost.TabIndex = 7;
             // 
-            // textBox7
+            // textNotes
             // 
-            this.textBox7.Location = new System.Drawing.Point(1017, 26);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(137, 23);
-            this.textBox7.TabIndex = 8;
+            this.textNotes.Location = new System.Drawing.Point(1017, 26);
+            this.textNotes.Name = "textNotes";
+            this.textNotes.Size = new System.Drawing.Size(137, 23);
+            this.textNotes.TabIndex = 8;
             // 
             // label1
             // 
@@ -180,14 +188,15 @@ namespace constructionformsapp
             this.label8.TabIndex = 16;
             this.label8.Text = "Notes";
             // 
-            // button1
+            // buttonSubmit
             // 
-            this.button1.Location = new System.Drawing.Point(1160, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 26);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Location = new System.Drawing.Point(1160, 23);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(80, 26);
+            this.buttonSubmit.TabIndex = 17;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
             // 
             // dataGridView1
             // 
@@ -195,7 +204,7 @@ namespace constructionformsapp
             this.dataGridView1.Location = new System.Drawing.Point(12, 55);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(1234, 570);
+            this.dataGridView1.Size = new System.Drawing.Size(1228, 482);
             this.dataGridView1.TabIndex = 18;
             // 
             // Form1
@@ -204,7 +213,7 @@ namespace constructionformsapp
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 637);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSubmit);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -213,14 +222,14 @@ namespace constructionformsapp
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textNotes);
+            this.Controls.Add(this.textCost);
+            this.Controls.Add(this.textUnitCost);
+            this.Controls.Add(this.textQuantity);
+            this.Controls.Add(this.textDescription);
+            this.Controls.Add(this.textMaterial);
+            this.Controls.Add(this.textItem);
+            this.Controls.Add(this.listCategory);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -231,14 +240,14 @@ namespace constructionformsapp
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ComboBox listCategory;
+        private System.Windows.Forms.TextBox textItem;
+        private System.Windows.Forms.TextBox textMaterial;
+        private System.Windows.Forms.TextBox textDescription;
+        private System.Windows.Forms.TextBox textQuantity;
+        private System.Windows.Forms.TextBox textUnitCost;
+        private System.Windows.Forms.TextBox textCost;
+        private System.Windows.Forms.TextBox textNotes;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -247,7 +256,7 @@ namespace constructionformsapp
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
